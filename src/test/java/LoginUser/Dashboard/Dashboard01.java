bdb12f2f45e4de22aa579a81f19eea8e6c860b6d
Vincent <vincentong1997@gmail.com>
2026-04-27 02:21:19 +0800

    initial commit

--- src/test/java/LoginUser/Dashboard/Dashboard01.java
+++ src/test/java/LoginUser/Dashboard/Dashboard01.java
@@ -23,7 +23,7 @@ public class Dashboard01 {
         // Set up ChromeDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // Required for Jenkins
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
